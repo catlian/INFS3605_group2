e4a31e60bd149dec0416283bca747ba5b9c41f42
catlian <cat.lian83@gmail.com>
2020-03-09 16:13:16 +1100

    Initial commit

---
+++ app/src/main/java/com/example/infs3605_group2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.infs3605_group2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
