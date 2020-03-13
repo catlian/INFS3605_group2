10e86dd46cabc194d861acfe2be3311c2cd867c2
catlian <cat.lian83@gmail.com>
2020-03-13 14:23:09 +1100

    basic setup - fin transaction class

--- app/src/main/java/com/example/infs3605_group2/Models/Transaction.java
+++ app/src/main/java/com/example/infs3605_group2/Models/Transaction.java
@@ -6,4 +6,31 @@ public class Transaction {
     private LocalDateTime timestamp;
     private String event;
     private String description;
+
+    public Transaction() {
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
