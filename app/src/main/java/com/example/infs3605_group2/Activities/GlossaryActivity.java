3941331a0a17e83827c9d21f09dfebf52d22bd01
JHOPE333 <44356529+JHOPE333@users.noreply.github.com>
2020-04-20 18:36:35 +1000

    Add files via upload
    Glossary activity

---
+++ app/src/main/java/com/example/infs3605_group2/Activities/GlossaryActivity.java
@@ -0,0 +1,16 @@
+package com.example.infs3605_group2.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.infs3605_group2.R;
+
+public class GlossaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_glossary);
+    }
+}
